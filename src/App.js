5fdedf859b0d2f37b1454d2c9db2d4fe08fb04ec
raja.k@relyonsoft.com-at-583467868209 <raja.k@relyonsoft.com>
2023-05-05 20:13:50 +0530

    File upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div className="WSP-header">
+        <img src='./images/WSPHeader.svg' />
+      </div>
+
+      <div className="wsp">
+      <div className="wsp-container">
+      {/* <p className="text-header">WSP</p>
+      <p className="text-title">Water Street Projects</p> */}
+      <p className="text-header">WSP</p>
+      <p className="text-title">Water Street Projects</p>
+      {/* <img src='./images/WSPtitle.svg' /> */}
+      <p className="text-descp">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et </p>
+      <input
+        className="input search"
+        name="name"
+        placeholder="Email"
+      /> 
+      <button type="button" className="button" style={{cursor: "pointer"}} >
+        <span style={{color: '#293C2B'}}>CONNECT</span>
+        </button >
+      </div>
     </div>
+    </>
   );
 }
 
